b8c6ac473955a1c47a99dd550b2afeb8bc24ab00
marcio72vieira <marcio72vieira@hotmail.com>
2024-06-18 19:13:24 -0300

    Links paginação incompleto

--- app/Repositories/SupportEloquentORM.php
+++ app/Repositories/SupportEloquentORM.php
@@ -19,17 +19,17 @@ class SupportEloquentORM implements SupportRepositoryInterface
     public function paginate(int $page = 1, int $totalPerPage = 15, string $filter = null): PaginationInterface
     {
         $result =  $this->model
-        ->where(function($query) use($filter){
-            if($filter){
-                $query->where('subject', $filter);
-                $query->orWhere('body', 'like', "%{$filter}%");
-            }
-        })
-        ->paginate($totalPerPage, ['*'], 'page', $page);
+            ->where(function($query) use($filter){
+                if($filter){
+                    $query->where('subject', $filter);
+                    $query->orWhere('body', 'like', "%{$filter}%");
+                }
+            })
+            ->paginate($totalPerPage, ['*'], 'page', $page);
 
-        dd($result);
+        //dd((new PaginationPresenter($result))->items());
 
-
+        return new PaginationPresenter($result);
     }
 
     public function getAll(string $filter = null): array
